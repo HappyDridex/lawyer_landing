1674065872d9e01e9e995162a6f86b2a8ab92f3d
dwarlin <dwarlin@mail.ru>
2023-06-28 03:20:11 +0400

    Финал

--- swiper.js
+++ swiper.js
@@ -1,6 +1,9 @@
 // swiper
+const slidesPerView = window.innerWidth < 768 ? (window.innerWidth < 500 ? 1 : 3) : 4.2;
+
 const swiper = new Swiper(".swiper", {
-    slidesPerView: 4,
+    slidesPerView,
+    spaceBetween: 30,
     initialSlide: 1,
     navigation: {
         nextEl: ".swiper-button-next",
