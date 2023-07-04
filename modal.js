effc9ecec68b5e36a9c96884820615910a0ce9f8
dwarlin <dwarlin@mail.ru>
2023-07-04 21:45:08 +0400

    Финал

---
+++ modal.js
@@ -0,0 +1,43 @@
+import { casesList } from "./cases-list.js";
+
+const modalWrapper = document.getElementById("modal-wrapper");
+const modalTitle = document.getElementById("modal-title");
+const modalText = document.getElementById("modal-text");
+const modalPrevSlideBtn = document.getElementById("modal-prev");
+const modalNextSlideBtn = document.getElementById("modal-next");
+const modalCloseBtn = document.getElementById("modal-close");
+let activeSlide;
+
+modalPrevSlideBtn.onclick = () => switchSlide(-1);
+modalNextSlideBtn.onclick = () => switchSlide(1);
+modalCloseBtn.onclick = (e) => hideModal(e);
+
+document.addEventListener("keydown", hideModal);
+
+document.querySelectorAll(".cases-swiper .swiper-slide").forEach((slide) => {
+    slide.addEventListener("click", (evt) => {
+        evt.stopPropagation();
+        activeSlide = casesList.find((caseEl) => caseEl.id === evt.currentTarget.id);
+        renderSlide();
+    });
+});
+
+function switchSlide(step) {
+    if (!activeSlide) return;
+    activeSlide =
+        casesList.find((caseEl) => +caseEl.id === +activeSlide.id + step) || casesList[0];
+    renderSlide();
+}
+
+function renderSlide() {
+    modalTitle.innerText = activeSlide?.title;
+    modalText.innerText = activeSlide?.body;
+    modalWrapper.style.display = "flex";
+    setTimeout(() => (modalWrapper.style.opacity = 1), 0);
+}
+
+function hideModal(evt) {
+    if (evt?.key === "Escape" || evt instanceof PointerEvent)
+        modalWrapper.style.opacity = 0;
+    setTimeout(() => (modalWrapper.style.display = "none"), 300);
+}
--- swiper.js
+++ swiper.js
@@ -1,16 +1,23 @@
 // swiper
-const slidesPerView = window.innerWidth < 768 ? (window.innerWidth < 500 ? 1 : 3) : 4.2;
 
-const swiper = new Swiper(".swiper", {
-    slidesPerView,
+const certificatesSwiper = new Swiper(".certificates-swiper", {
+    slidesPerView: window.innerWidth < 768 ? (window.innerWidth < 500 ? 1 : 3) : 4.2,
     spaceBetween: 30,
     initialSlide: 1,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".next-certificate-button",
+        prevEl: ".prev-certificate-button",
     },
     zoom: { maxRatio: 2, minRatio: 1 },
 });
 
-const prevSlide = () => swiper.slidePrev();
-const nextSlide = () => swiper.slideNext();
+const casesSwiper = new Swiper(".cases-swiper", {
+    slidesPerView: window.innerWidth < 768 ? 1 : 2,
+    spaceBetween: 60,
+    initialSlide: 0,
+    navigation: {
+        nextEl: ".next-case-button",
+        prevEl: ".prev-case-button",
+    },
+    zoom: { maxRatio: 2, minRatio: 1 },
+});
